2777938df21c58f8e447c3d1c4b49ba899d2367e
S4nop <rshtiger@naver.com>
2020-05-23 16:12:14 +0900

    git ignore added

--- worker.py
+++
@@ -1,7 +0,0 @@
-import military_letter_worker as mlw
-import functions_area
-
-
-worker = mlw.AutoBodyMaker("sanop")
-print(worker.makeBody())
-#worker.sendLetter("rshtiger@naver.com", "qwer123$", "김명수", "0518소식")
